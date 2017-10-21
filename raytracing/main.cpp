2663e9444d4f876dc7ef7fc47b95c0ddd6c231bf
tianzigeng <250445881@qq.com>
2017-10-21 12:29:15 +0800

    fix_perspective

--- raytracing/main.cpp
+++ raytracing/main.cpp
@@ -16,13 +16,13 @@ int main() {
 	World world;
 	world.build();
 
-	RGBColor pixel_color;
+	RGBColor pixel_color; 
 	Ray ray;
-	double zw = 100;
+	double zw = -100;
 	Vec2 sp, pp;
 	int sample_num = world.vp.get_samples_num();
 
-	ray.direction = Vec3(0, 0, -1);
+	ray.origin =Vec3(0, 0, 150);
 
 	while (render.Run()) {
 		hdc = BeginPaint(window->GetHandler(), &window->GetPainter());
@@ -31,9 +31,9 @@ int main() {
 				pixel_color = RGBColor(0, 0, 0);
 				for (int s = 0; s != sample_num; ++s) {
 					sp = world.vp.sampler_ptr->sample_unit_square();
-					pp.x = world.vp.pixel_size*(i - 0.5*world.vp.hres + sp.x);
-					pp.y = world.vp.pixel_size*(j - 0.5*world.vp.vres + sp.y);
-					ray.origin = Vec3(pp.x, pp.y, zw);
+					pp.x = world.vp.pixel_size*(i - 0.5*(world.vp.hres - 1) + sp.x);
+					pp.y = world.vp.pixel_size*(j - 0.5*(world.vp.vres - 1) + sp.y);
+					ray.direction = Vec3(pp.x, pp.y, zw);
 					pixel_color += world.tracer_ptr->trace_ray(ray);
 				}
 				pixel_color /= sample_num;
